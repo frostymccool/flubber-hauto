da3e38e8d618c724f31cef7ab0d4c3024663e40d
mefrosty <ian.t.shaw@gmail.com>
2012-12-15 00:10:18 +0000

    add parameter for heating 1 for hub, 2 for heating, 3 for both

--- heating/heating.sh
+++ heating/heating.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
+# kick off script
+# params:
+#    1 to kick of the xap-hub
+#    2 to kick of heating controller
 
-/home/common/heating/xap_pollthermostats.py
+
+cd /home/common/heating
+
+if [ "$1" = "1" ]; then
+    echo "kick off hub"
+    ./xap-hub &
+fi
+
+if [ "$1" = "2" ]; then
+    echo "kick off heating controller"
+    python xap_pollthermostats.py
+fi
+
+if [ "$1" = "3" ]; then
+    echo "kick off both"
+    ./xap-hub &
+    python xap_pollthermostats.py
+fi
